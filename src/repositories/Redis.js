f82122840024fed84611b77484be300f69644b86
Arthur Kh <listbox6@gmail.com>
2022-11-01 22:13:04 +0200

    feat: added redis retry options

--- src/repositories/Redis.js
+++ src/repositories/Redis.js
@@ -5,7 +5,15 @@ import { logger } from "../logger.js";
 
 const REDIS_URL = config("REDIS_URL");
 
-export const redisClient = new IORedis(REDIS_URL);
+export const redisClient = new IORedis(REDIS_URL, {
+  maxRetriesPerRequest: 30,
+  retryStrategy(times) {
+    return Math.min(times * 50, 2000);
+  },
+  reconnectOnError() {
+    return true;
+  },
+});
 
 redisClient.on("error", (error) => {
   logger.error("[redis]", error);

ea9f83ba8f5fa5d10ba7856763ba96dc5c585138
Arthur Kh <listbox6@gmail.com>
2022-11-03 01:32:02 +0200

    feat: added cache service

---
+++ src/services/CacheService.js
@@ -0,0 +1,37 @@
+import { RedisStringMap } from "../types/RedisStringMap.js";
+
+export class CacheService {
+  constructor() {
+    this.map = new RedisStringMap("cache");
+  }
+
+  /**
+   * @param {string} key
+   */
+  async get(key) {
+    const value = await this.map.get(key);
+    if (value === null || value === undefined) return null;
+    return JSON.parse(value);
+  }
+
+  /**
+   *
+   * @param {string} key
+   * @param {any} value
+   * @returns
+   */
+  set(key, value) {
+    return this.map.set(key, JSON.stringify(value));
+  }
+
+  /**
+   *
+   * @param {string} key
+   * @param {number} ttl
+   * @param {any} value
+   * @returns
+   */
+  setex(key, ttl, value) {
+    return this.map.setex(key, ttl, JSON.stringify(value));
+  }
+}

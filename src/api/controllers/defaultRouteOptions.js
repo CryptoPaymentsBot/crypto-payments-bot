d151fb42d9eeb3ec117a24d8171cc3a86f6d0519
Arthur Kh <listbox6@gmail.com>
2022-11-03 01:35:33 +0200

    feat: added default route type

---
+++ src/api/controllers/defaultRouteOptions.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {import("fastify").RouteOptions<import("fastify").RawServerDefault, import("fastify").RawRequestDefaultExpression, import("fastify").RawReplyDefaultExpression<import("fastify").RawServerDefault>, T>} DefaultRouteOptions
+ *
+ * @template T
+ */
+export default {};
